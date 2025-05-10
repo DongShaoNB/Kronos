12521ab9da968df721fac796593e20e0249bf2e1
DongShaoNB <54093566+DongShaoNB@users.noreply.github.com>
2025-05-10 21:39:11 +0800

    perf: remove TODO and change user table language from varchar to char

--- src/main/kotlin/cc/dsnb/database/table/UserTable.kt
+++ src/main/kotlin/cc/dsnb/database/table/UserTable.kt
@@ -8,15 +8,13 @@ import org.jetbrains.exposed.sql.javatime.datetime
 object UserTable : IntIdTable("users") {
 
     val username = varchar("username", 50).uniqueIndex()
-
-    // TODO
     val avatar = varchar("avatar", 255).default("default/default.png")
     val name = varchar("name", 50)
     val email = varchar("email", 255).uniqueIndex()
     val emailVerified = bool("email_verified").default(false)
     val roleId =
         reference("role_id", RoleTable.id, onUpdate = ReferenceOption.CASCADE, onDelete = ReferenceOption.RESTRICT)
-    val language = varchar("language", 2).default("zh")
+    val language = char("language", 2).default("zh")
     val passwordHash = varchar("password_hash", 60)
     val registerIp = varchar("register_ip", 15)
     val registeredAt = datetime("registered_at").defaultExpression(CurrentDateTime)
